84c1c16af43eee6c40d1bb09ae3851f12225103d
Samuel7571 <samuel_sza@outlook.com.br>
2025-06-20 00:28:04 -0300

    Add .next/static/chunks/src_app_dashboard_performance_page_tsx_b8451d02._.js 1750390084413

---
+++ .next/static/chunks/src_app_dashboard_performance_page_tsx_b8451d02._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_ba6edfc4._.js",
+  "static/chunks/node_modules_acaee6e5._.js"
+],
+    source: "dynamic"
+});
